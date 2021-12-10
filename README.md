3f9a48242f9a91ae3fc25f8647f68dc1fa29c76b
ahmed <aabdulbasset5@gmail.com>
2021-12-10 18:56:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Hello, I'm Ahmed! 
+And this is my project submission on the professional web track.
+
+
+# Landing page project
+
+A responsive(90%) and dynamic landing page.
+
+
+
+## Usage/Examples
+
+```html
+Run in your browser
+Will work natively
+```
+
+
+## Dependencies
+
+This project is built using CSS(with animations), HTML, and Vanilla javascript.
+
+A modern browser is recommended for the best experience and for some of the animations to work
+
---
+++ js/app.js
@@ -0,0 +1,83 @@
+const sections = document.querySelectorAll("section")
+//Build the navigation menu
+sections.forEach(function(item){
+    var elem = document.createElement("li")
+    
+    elem.appendChild(document.createTextNode(item.dataset.nav))
+    elem.className = "menu__link"
+    elem.setAttribute("data-nav",item.dataset.nav)
+    document.querySelector("#navbar__list").appendChild(elem)
+    //Dynamic Collapse button
+    let collapse = document.createElement("div")
+    let collapseIcon = document.createElement("img")
+    collapseIcon.setAttribute("src","https://cdn-icons-png.flaticon.com/512/32/32195.png")
+    collapse.appendChild(collapseIcon)
+    collapse.setAttribute("data-collapse",item.dataset.nav)
+    collapse.setAttribute("onclick","hideSection(this)")
+    collapse.className="collapse-button"
+    item.insertAdjacentElement('afterbegin',collapse)
+    
+})
+
+function hideSection(elmnt){
+    sectionToCollap = document.querySelector(`main > [data-nav="${elmnt.dataset.collapse}"]`)
+    textToHide = sectionToCollap.querySelectorAll(".landing__container>p")
+    textToHide.forEach(function(e){
+        if(e.style.opacity=="0"){
+            e.style.opacity = "1"
+        }else{
+            e.style.opacity = "0"
+        }
+    })
+        
+    
+}
+//Get the viewport and add/remove the "active" class
+var scrollingNav,scrollingSection;
+document.addEventListener("scroll",function(e){
+    
+    document.querySelector(".page__header").style.opacity = "1"
+    window.clearTimeout( scrollingNav );
+    window.clearTimeout(scrollingSection )
+    //Make it run every 200ms only
+    scrollingSection = setTimeout(function(){
+        if(window.scrollY+window.innerHeight >= document.body.scrollHeight/2){
+            document.querySelector(".scroll-to-top").style.display = "block"
+        }else{
+            document.querySelector(".scroll-to-top").style.display = "none"
+        }
+        sections.forEach(function(section){
+        
+            position = section.getBoundingClientRect()
+            if(position.y > -100 && position.y < position.height/1.2){
+                section.classList.add("active")
+                document.querySelector(`[data-nav="${section.dataset.nav}"]`).classList.add("active")
+            }else{
+                section.classList.remove("active")
+                document.querySelector(`[data-nav="${section.dataset.nav}"]`).classList.remove("active")
+            }
+        })
+    },200)
+
+    //Hide navbar when user stops scrolling
+    scrollingNav = setTimeout(function(){
+        document.querySelector(".page__header").style.opacity = "0"
+    },3000)
+})
+//Make navbar scroll to sections
+document.querySelector("ul").addEventListener("click",function(e){
+    if(e.target.tagName == "LI"){
+        position = document.querySelector(`main>[data-nav="${e.target.dataset.nav}"]`).getBoundingClientRect()
+        window.scrollBy(
+            {
+                top:position.y,
+                left:position.x,
+                behavior: 'smooth'
+            }
+        )
+    }
+})
+//Make navbar appear on hover
+document.querySelector(".page__header").addEventListener("mouseover",function(e){
+    document.querySelector(".page__header").style.opacity="1"
+})
